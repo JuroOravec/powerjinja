6df6b82112eead57a1bf479b0b97a14db3c66b62
Juro Oravec <juraj.oravec.josefson@gmail.com>
2018-10-28 13:52:29 +0000

    Update interface in create-instance.ts

--- packages/powerjinja-core/src/create-instance.ts
+++ packages/powerjinja-core/src/create-instance.ts
@@ -1,12 +1,13 @@
 import { init } from "inject-definition";
+import { DefinitionInjector } from "inject-definition/lib/interface";
 
 import { minifier } from "./config/minifier";
 import { variableNameRetriever } from "./config/variable-name-retriever";
 import { variableNameReplacer } from "./config/variable-name-replacer";
 import { declarationFormatter } from "./config/declaration-formatter";
 
-export function createInstance(definitions?: object) {
-  init({
+export function createInstance(definitions?: DefinitionInjector.IDefinition) {
+  return init({
     definitions,
     minifier,
     variableNameReplacer,
