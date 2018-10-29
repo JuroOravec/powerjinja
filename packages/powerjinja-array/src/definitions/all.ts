55316f85d2447a8648e74700c0faf99fd266e948
Juro Oravec <juraj.oravec.josefson@gmail.com>
2018-10-29 19:17:40 +0000

    Document powerjinja-array definitions

--- packages/powerjinja-array/src/definitions/all.ts
+++ packages/powerjinja-array/src/definitions/all.ts
@@ -1,6 +1,18 @@
 // Declared dependencies for housekeeping
 import { handleQueue } from "powerjinja-core/lib/definitions/handle-queue";
 
+/**
+ * Returns true iff all items in an iterable are truthy.
+ * @param {iterable} list any iterable
+ * @returns {boolean}
+ * @example
+ * {{- powerjinja.array.all([1, 'hello', [1]], macros=[powerjinja.core.print]) -}}
+ * // prints True
+ *
+ * @example
+ * {{- powerjinja.array.all([1, 'hello', [1], {}], macros=[powerjinja.core.print]) -}}
+ * // prints False
+ */
 export const all = `
 {%- macro all(arg1=[], arg2=None, arg3=None, arg4=None, arg5=None, macros=[], index=0) -%}
 	{%- falses = [] -%}
--- packages/powerjinja-array/src/definitions/any.ts
+++ packages/powerjinja-array/src/definitions/any.ts
@@ -1,6 +1,18 @@
 // Declared dependencies for housekeeping
 import { handleQueue } from "powerjinja-core/lib/definitions/handle-queue";
 
+/**
+ * Returns true if any of the items in an iterable is truthy.
+ * @param {iterable} list any iterable
+ * @returns {boolean}
+ * @example
+ * {{- powerjinja.array.any([1, '', [], {}], macros=[powerjinja.core.print]) -}}
+ * // prints True
+ *
+ * @example
+ * {{- powerjinja.array.any([0, '', [], {}], macros=[powerjinja.core.print]) -}}
+ * // prints False
+ */
 export const any = `
 {%- macro any(arg1=[], arg2=None, arg3=None, arg4=None, arg5=None, macros=[], index=0) -%}
 	{%- trues = [] -%}
