8b4336168de91c6958b42705b73ca832ffc60db9
Juro Oravec <juraj.oravec.josefson@gmail.com>
2018-10-30 17:46:01 +0000

    Rename "list" to "array"

--- packages/powerjinja-assert/src/definitions/value/is-array.ts
+++ packages/powerjinja-assert/src/definitions/value/is-array.ts
@@ -6,15 +6,15 @@ import { handleQueue } from "powerjinja-core/lib/definitions/handle-queue";
  * @param {any} arg value that is being checked
  * @returns {boolean}
  * @example
- * {{- powerjinja.assert.value.is_list(0, macros=[powerjinja.core.print]) -}}
+ * {{- powerjinja.assert.value.is_array(0, macros=[powerjinja.core.print]) -}}
  * // prints False
  *
  * @example
- * {{- powerjinja.assert.value.is_list([], macros=[powerjinja.core.print]) -}}
+ * {{- powerjinja.assert.value.is_array([], macros=[powerjinja.core.print]) -}}
  * // prints True
  */
-export const isList = `
-{%- macro is_list(arg1=None, arg2=None, arg3=None, arg4=None, arg5=None, macros=[], index=0) -%}
+export const isArray = `
+{%- macro is_array(arg1=None, arg2=None, arg3=None, arg4=None, arg5=None, macros=[], index=0) -%}
 	{%- set result = arg1 is iterable and arg1 is not string and arg1 is not mapping -%}
 	{{- powerjinja.core.handle_queue(result, arg2, arg3, arg4, arg5, macros, index) -}}
 {%- endmacro -%}
