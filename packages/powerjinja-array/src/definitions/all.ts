9d2fe6f802ebbb7909f26026ec9090849c4f107b
Juro Oravec <juraj.oravec.josefson@gmail.com>
2018-10-30 18:15:44 +0000

    Fix typo in powerjinja.array.all

--- packages/powerjinja-array/src/definitions/all.ts
+++ packages/powerjinja-array/src/definitions/all.ts
@@ -15,7 +15,7 @@ import { handleQueue } from "powerjinja-core/lib/definitions/handle-queue";
  */
 export const all = `
 {%- macro all(arg1=[], arg2=None, arg3=None, arg4=None, arg5=None, macros=[], index=0) -%}
-	{%- falses = [] -%}
+	{%- set falses = [] -%}
 	{%- for i in arg1 -%}
 		{%- if not i -%}
 			{%- append i to falses -%}
