6cb5c30fb0765a1f4b9eaef29a0af25667aba025
Juro Oravec <juraj.oravec.josefson@gmail.com>
2018-11-03 11:13:11 +0000

    remove getters/setter from default syntax object

--- packages/powerjinja-core/src/config/defaultSyntax.ts
+++ packages/powerjinja-core/src/config/defaultSyntax.ts
@@ -2,63 +2,57 @@ import { Powerjinja } from "../interface/powerjinja";
 
 export function defaultSyntax(): Powerjinja["config"] {
   return {
-    get delimeter() {
-      return {
-        statementOpen: () => "{%",
-        statementOpenTrim: () => "{%-",
-        statementOpenNoTrim: () => "{%+",
-        statementClose: () => "%}",
-        statementCloseTrim: () => "-%}",
-        statementCloseNoTrim: () => "+%}",
-        printOpen: () => "{{",
-        printOpenTrim: () => "{{-",
-        printOpenNoTrim: () => "{{+",
-        printClose: () => "}}",
-        printCloseTrim: () => "-}}",
-        printCloseNoTrim: () => "+}}",
-        commentOpen: () => "{#",
-        commentOpenTrim: () => "{#-",
-        commentOpenNoTrim: () => "{#+",
-        commentClose: () => "#}",
-        commentCloseTrim: () => "-#}",
-        commentCloseNoTrim: () => "+#}",
-        lineStatementOpen: () => "#",
-        lineStatementOpenTrim: () => "#-",
-        lineStatementOpenNoTrim: () => "#+",
-        lineStatementClose: () => "##",
-        lineStatementCloseTrim: () => "-##",
-        lineStatementCloseNoTrim: () => "+##"
-      };
+    delimeter: {
+      statementOpen: () => "{%",
+      statementOpenTrim: () => "{%-",
+      statementOpenNoTrim: () => "{%+",
+      statementClose: () => "%}",
+      statementCloseTrim: () => "-%}",
+      statementCloseNoTrim: () => "+%}",
+      printOpen: () => "{{",
+      printOpenTrim: () => "{{-",
+      printOpenNoTrim: () => "{{+",
+      printClose: () => "}}",
+      printCloseTrim: () => "-}}",
+      printCloseNoTrim: () => "+}}",
+      commentOpen: () => "{#",
+      commentOpenTrim: () => "{#-",
+      commentOpenNoTrim: () => "{#+",
+      commentClose: () => "#}",
+      commentCloseTrim: () => "-#}",
+      commentCloseNoTrim: () => "+#}",
+      lineStatementOpen: () => "#",
+      lineStatementOpenTrim: () => "#-",
+      lineStatementOpenNoTrim: () => "#+",
+      lineStatementClose: () => "##",
+      lineStatementCloseTrim: () => "-##",
+      lineStatementCloseNoTrim: () => "+##"
     },
-    get tag() {
-      return {
-        set: () => "set",
-        endset: () => "endset",
-        block: () => "block",
-        endblock: () => "endblock",
-        call: () => "call",
-        endcall: () => "endcall",
-        do: () => "do",
-        extends: () => "extends",
-        filter: () => "filter",
-        endfilter: () => "endfilter",
-        if: () => "if",
-        else: () => "else",
-        elif: () => "elif",
-        endif: () => "endif",
-        include: () => "include",
-        import: () => "import",
-        for: () => "for",
-        endfor: () => "endfor",
-        from: () => "from",
-        macro: () => "macro",
-        endmacro: () => "endmacro"
-      };
+    tag: {
+      set: () => "set",
+      endset: () => "endset",
+      block: () => "block",
+      endblock: () => "endblock",
+      call: () => "call",
+      endcall: () => "endcall",
+      do: () => "do",
+      extends: () => "extends",
+      filter: () => "filter",
+      endfilter: () => "endfilter",
+      if: () => "if",
+      else: () => "else",
+      elif: () => "elif",
+      endif: () => "endif",
+      include: () => "include",
+      import: () => "import",
+      for: () => "for",
+      endfor: () => "endfor",
+      from: () => "from",
+      macro: () => "macro",
+      endmacro: () => "endmacro"
     },
-    get command() {
-      return {
-        append: (list, item) => `${this.tag.do()} ${list}.append(${item})`
-      };
+    command: {
+      append: (list, item) => `do ${list}.append(${item})`
     }
   };
 }
